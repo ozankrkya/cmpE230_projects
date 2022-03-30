647bcb20e343de60c5a529067f76e4f204af3b4a
Alibasarann <ali.basaran@boun.edu.tr>
2022-03-30 18:02:11 +0300

    structs file is opened and minor changes has been made

--- functions.c
+++ functions.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-;
 // checks whether the token is reserved by matlang
 bool isReserved(int c, int arr[]){
     for(int i = 0; i < sizeof(arr)/sizeof(char); i++){
